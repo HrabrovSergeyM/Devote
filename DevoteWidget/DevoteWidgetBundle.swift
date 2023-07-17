68ed48707c3fc4f5b3b44c56242c81ae9a4707e1
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-07-17 18:07:26 +0300

    widget: Added widget template

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by Sergey Hrabrov on 17.07.2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+    }
+}
